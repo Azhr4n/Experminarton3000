0620fb6fc99b08ace4b24f57bb6dbf288674b22f
U-AZHRAN\Azhran <firefrai@gmail.com>
2016-04-14 16:16:48 +0200

    added lot of shit.

--- server.py
+++ server.py
@@ -1,29 +1,25 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
-def callMagic():
-	print 'called !'
-
 clients = []
-class SimpleServer(WebSocket):
+class SimpleChat(WebSocket):
 
 	def handleMessage(self):
-
-		if self.data == '/Magic':
-			callMagic()
-
 		for client in clients:
 			if client != self:
 				client.sendMessage(self.address[0] + u' - ' + self.data)
+		print self.data
 
 	def handleConnected(self):
-		for client in clients:
-			client.sendMessage(self.address[0] + u' - connected.')
-		clients.append(self)
 		print self.address, 'connected'
+		for client in clients:
+			client.sendMessage(self.address[0] + u' - connected')
+		clients.append(self)
 
 	def handleClose(self):
+		clients.remove(self)
 		print self.address, 'closed'
+		for client in clients:
+			client.sendMessage(self.address[0] + u' - disconnected')
 
-
-server = SimpleWebSocketServer('', 5000, SimpleServer)
+server = SimpleWebSocketServer('', 8000, SimpleChat)
 server.serveforever()
