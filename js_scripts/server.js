af5d081a1982b9f134fabe8458a6cfb1919af951
Azhr4n <firefrai@gmail.com>
2016-04-18 12:33:42 +0200

    added bootstrap

--- js_scripts/server.js
+++ js_scripts/server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(express.static('static'));
 
 app.get('/', function(req, res) {
-	res.render('index.ejs');
+	res.render('index.twig');
 });
 
 app.get('/get_data', function(req, res) {
-	res.render('get_data.ejs');
+	res.render('get_data.twig');
 });
 
 app.listen(8080);
